3f030bd58a11a30b957a69da91037b9385626034
Duong Van Nhat <nhatn42003bn@gmail.com>
2023-10-14 11:17:04 +0700

    .

--- projectJava/lab1/hello.java
+++ projectJava/lab1/hello.java
@@ -2,7 +2,6 @@ package projectJava.lab1;
 
 public class hello {
     public static void main(String[] args) {
-        // in "hello HowKteam" tren terminnal window.
         System.out.println("hello howKteam");
      }
 }
